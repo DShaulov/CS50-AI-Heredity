8d637ff381041d4513ade53ca68ea66be888a358
DShaulov <shaulovl55@gmail.com>
2020-09-25 14:21:17 +0300

    Final - fixed

--- joint-probability-test.py
+++ joint-probability-test.py
@@ -1,7 +1,3 @@
-import csv
-import itertools
-import sys
-
 PROBS = {
 
     # Unconditional probabilities for having gene
@@ -37,95 +33,6 @@ PROBS = {
 }
 
 
-def main():
-
-    # Check for proper usage
-    if len(sys.argv) != 2:
-        sys.exit("Usage: python heredity.py data.csv")
-    people = load_data(sys.argv[1])
-
-    # Keep track of gene and trait probabilities for each person
-    probabilities = {
-        person: {
-            "gene": {
-                2: 0,
-                1: 0,
-                0: 0
-            },
-            "trait": {
-                True: 0,
-                False: 0
-            }
-        }
-        for person in people
-    }
-
-    # Loop over all sets of people who might have the trait
-    names = set(people)
-    for have_trait in powerset(names):
-
-        # Check if current set of people violates known information
-        fails_evidence = any(
-            (people[person]["trait"] is not None and
-             people[person]["trait"] != (person in have_trait))
-            for person in names
-        )
-        if fails_evidence:
-            continue
-
-        # Loop over all sets of people who might have the gene
-        for one_gene in powerset(names):
-            for two_genes in powerset(names - one_gene):
-
-                # Update probabilities with new joint probability
-                p = joint_probability(people, one_gene, two_genes, have_trait)
-                update(probabilities, one_gene, two_genes, have_trait, p)
-
-    # Ensure probabilities sum to 1
-    normalize(probabilities)
-
-    # Print results
-    for person in people:
-        print(f"{person}:")
-        for field in probabilities[person]:
-            print(f"  {field.capitalize()}:")
-            for value in probabilities[person][field]:
-                p = probabilities[person][field][value]
-                print(f"    {value}: {p:.4f}")
-
-
-def load_data(filename):
-    """
-    Load gene and trait data from a file into a dictionary.
-    File assumed to be a CSV containing fields name, mother, father, trait.
-    mother, father must both be blank, or both be valid names in the CSV.
-    trait should be 0 or 1 if trait is known, blank otherwise.
-    """
-    data = dict()
-    with open(filename) as f:
-        reader = csv.DictReader(f)
-        for row in reader:
-            name = row["name"]
-            data[name] = {
-                "name": name,
-                "mother": row["mother"] or None,
-                "father": row["father"] or None,
-                "trait": (True if row["trait"] == "1" else
-                          False if row["trait"] == "0" else None)
-            }
-    return data
-
-
-def powerset(s):
-    """
-    Return a list of all possible subsets of set s.
-    """
-    s = list(s)
-    return [
-        set(s) for s in itertools.chain.from_iterable(
-            itertools.combinations(s, r) for r in range(len(s) + 1)
-        )
-    ]
 
 
 def joint_probability(people, one_gene, two_genes, have_trait):
@@ -156,7 +63,7 @@ def joint_probability(people, one_gene, two_genes, have_trait):
         # either person gets the gene from his mother and not his father
         # or he gets it from his father and not his mother
         if person_father in one_gene:
-            father_passing_probability = (0.5 * 0.99) + (0.5 * 0.01)  # passes sick gene and it doesnt mutate OR passes healthy gene and it mutates
+            father_passing_probability = 0.49 # 0.5 - 0.01 (the probability of mutation to not the specified gene)
         elif person_father in two_genes:
             father_passing_probability = 0.99 # 1 - 0.01 (the probability of mutation to not the specified gene)
         else:
@@ -164,7 +71,7 @@ def joint_probability(people, one_gene, two_genes, have_trait):
 
 
         if person_mother in one_gene:
-            mother_passing_probability = (0.5 * 0.99) + (0.5 * 0.01) # passes sick gene and it doesnt mutate OR passes healthy gene and it mutates
+            mother_passing_probability = 0.49 # 0.5 - 0.01 (the probability of mutation to not the specified gene)
         elif person_mother in two_genes:
             mother_passing_probability = 0.99 # 1 - 0.01 (the probability of mutation to not the specified gene)
         else:
@@ -189,7 +96,7 @@ def joint_probability(people, one_gene, two_genes, have_trait):
 
         # check probability of passing the gene by persons parents
         if person_father in one_gene:
-            father_passing_probability = (0.5 * 0.99) + (0.5 * 0.01) # passes sick gene and it doesnt mutate OR passes healthy gene and it mutates
+            father_passing_probability = 0.49 # 0.5 - 0.01 (the probability of mutation to not the specified gene)
         elif person_father in two_genes:
             father_passing_probability = 0.99 # 1 - 0.01 (the probability of mutation to not the specified gene)
         else:
@@ -197,8 +104,8 @@ def joint_probability(people, one_gene, two_genes, have_trait):
 
 
         if person_mother in one_gene:
-            mother_passing_probability = (0.5 * 0.99) + (0.5 * 0.01) # passes sick gene and it doesnt mutate OR passes healthy gene and it mutates
-        elif person_mother in two_genes:
+            mother_passing_probability = 0.49 # 0.5 - 0.01 (the probability of mutation to not the specified gene)
+        elif person_father in two_genes:
             mother_passing_probability = 0.99 # 1 - 0.01 (the probability of mutation to not the specified gene)
         else:
             mother_passing_probability = 0.01 # (the probability of not the specified gene mutating into the specified gene)
@@ -228,7 +135,7 @@ def joint_probability(people, one_gene, two_genes, have_trait):
         
         # check probability of passing the gene by persons parents
         if person_father in one_gene:
-            father_passing_probability = (0.5 * 0.99) + (0.5 * 0.01) # passes sick gene and it doesnt mutate OR passes healthy gene and it mutates
+            father_passing_probability = 0.49 # 0.5 - 0.01 (the probability of mutation to not the specified gene)
         elif person_father in two_genes:
             father_passing_probability = 0.99 # 1 - 0.01 (the probability of mutation to not the specified gene)
         else:
@@ -236,7 +143,7 @@ def joint_probability(people, one_gene, two_genes, have_trait):
 
 
         if person_mother in one_gene:
-            mother_passing_probability = (0.5 * 0.99) + (0.5 * 0.01) # 0.5 - 0.01 (the probability of mutation to not the specified gene)
+            mother_passing_probability = 0.49 # 0.5 - 0.01 (the probability of mutation to not the specified gene)
         elif person_mother in two_genes:
             mother_passing_probability = 0.99 # 1 - 0.01 (the probability of mutation to not the specified gene)
         else:
@@ -290,47 +197,17 @@ def joint_probability(people, one_gene, two_genes, have_trait):
 
     return final_probability
 
-
-def update(probabilities, one_gene, two_genes, have_trait, p):
-    """
-    Add to `probabilities` a new joint probability `p`.
-    Each person should have their "gene" and "trait" distributions updated.
-    Which value for each distribution is updated depends on whether
-    the person is in `have_gene` and `have_trait`, respectively.
-    """
-    for person in probabilities:
-        if person not in one_gene and person not in two_genes:
-            probabilities[person]['gene'][0] = probabilities[person]['gene'][0] + p
-
-        if person in one_gene:
-            probabilities[person]['gene'][1] = probabilities[person]['gene'][1] + p
-
-        if person in two_genes:
-            probabilities[person]['gene'][2] = probabilities[person]['gene'][2] + p
-        
-        if person in have_trait:
-            probabilities[person]['trait'][True] = probabilities[person]['trait'][True] + p
-
-        if person not in have_trait:
-            probabilities[person]['trait'][False] = probabilities[person]['trait'][False] + p
+if __name__ == '__main__':
+    people = {
+    'Harry': {'name': 'Harry', 'mother': 'Lily', 'father': 'James', 'trait': None},
+    'James': {'name': 'James', 'mother': None, 'father': None, 'trait': True},
+    'Lily': {'name': 'Lily', 'mother': None, 'father': None, 'trait': False}
+    }
+    print(joint_probability(people, {"Harry","Lily"}, {"James"}, {"Harry", "Lily", "James"}))
 
 
 
-def normalize(probabilities):
-    """
-    Update `probabilities` such that each probability distribution
-    is normalized (i.e., sums to 1, with relative proportions the same).
-    """
-    for person in probabilities:
-        for gene_trait in probabilities[person]:
-            total_distribution = 0
-            for category in probabilities[person][gene_trait]:
-                total_distribution = total_distribution + probabilities[person][gene_trait][category]
-
-            for category in probabilities[person][gene_trait]:
-                probabilities[person][gene_trait][category] = float(probabilities[person][gene_trait][category]) / total_distribution
 
 
 
-if __name__ == "__main__":
-    main()
+
